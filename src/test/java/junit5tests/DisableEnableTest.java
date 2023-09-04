a9dcb151b6edf87e1217ac5cb28aa18c35282ad5
Aaron Flynn <aaron.flynn@brightpearl.com>
2023-09-04 15:21:20 +0100

    Chapter 10 Disabling and Enabling tests.

---
+++ src/test/java/junit5tests/DisableEnableTest.java
@@ -0,0 +1,51 @@
+package junit5tests;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.condition.*;
+
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class DisableEnableTest {
+
+  @Test
+  @Disabled("Disabled for demo purposes")
+  @Order(1)
+  void firstTest(){
+    System.out.println("First test method");
+  }
+
+  @Test
+  @Order(2)
+  @DisabledOnOs(value = OS.MAC, disabledReason = "DEMO")
+  void secondTest(){
+    System.out.println("Second test method");
+  }
+
+  @Test
+  @Order(3)
+  @DisabledIfSystemProperty(named = "ENV", matches = "staging", disabledReason = "Demo")
+  void thirdTest(){
+    System.out.println("Third test method");
+  }
+
+
+  boolean provider(){
+    return LocalDateTime.now().getDayOfWeek().equals(DayOfWeek.MONDAY);
+  }
+
+  @Test
+  @Order(4)
+  @DisabledIf(value = "provider", disabledReason = "Don\'t run on Monday")
+  void forthTest(){
+    System.out.println("Forth test method");
+  }
+
+  @Test
+  @Order(5)
+  @EnabledOnOs(OS.MAC)
+  void fifthTest(){
+    System.out.println("Fifth test method");
+  }
+}
