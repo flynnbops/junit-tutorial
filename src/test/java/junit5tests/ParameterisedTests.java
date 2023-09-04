361706c13e2cc1743d138c9b2c012a5a14c37f0a
Aaron Flynn <aaron.flynn@brightpearl.com>
2023-09-04 12:04:29 +0100

    Chapter 5 parameterised tests with @CSVSource

--- src/test/java/junit5tests/ParameterisedTests.java
+++ src/test/java/junit5tests/ParameterisedTests.java
@@ -2,10 +2,7 @@ package junit5tests;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EmptySource;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
-import org.junit.jupiter.params.provider.NullSource;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.*;
 
 public class ParameterisedTests {
 
@@ -22,5 +19,33 @@ public class ParameterisedTests {
   @ValueSource(strings = {"first", "second"})
   void StringValues(String params){
     System.out.println("params = " + params);
+  }
+
+  @DisplayName("Can use CSVSource Parameterized tests")
+  @ParameterizedTest
+  @CsvSource(value = {"john,doe", "ann,bowling", "jar,binks"})
+  void csvSourceStringString(String param1, String param2){
+    System.out.println("param1 = " + param1 + ", param2 = " + param2);
+  }
+
+  @DisplayName("Can use mixed types with CSVSource Parameterized tests")
+  @ParameterizedTest
+  @CsvSource(value = {"steve,32,true", "roger,12,false", "tom,1,true"})
+  void csvSourceMixedParameterTypes(String param1, int param2, boolean param3){
+    System.out.println("param1 = " + param1 + ", param2 = " + param2 + ", param3 = " + param3);
+  }
+
+  @DisplayName("Can use comma separated string as a single parameter")
+  @ParameterizedTest
+  @CsvSource(value = {"steve rodgers, 'comma,separated'", "jane doe, 'some,thing'"})
+  void csvSourceStringWithComma(String param1, String param2){
+    System.out.println("param1 = " + param1 + ", param2 = " + param2);
+  }
+
+  @DisplayName("Can change the CSV delimiter")
+  @ParameterizedTest
+  @CsvSource(value = {"steve?rodgers","some?thing"}, delimiter = '?')
+  void csvSourceChangeDelimiter(String param1, String param2){
+    System.out.println("param1 = " + param1 + ", param2 = " + param2);
   }
 }
